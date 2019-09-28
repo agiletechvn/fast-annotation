8efffd11fe95cd43c27d682d6db7887bad7ad8b7
Thanh Tu <tu@novalex.vn>
2019-09-28 22:55:01 +0700

    update

--- src/libs/WorkImage.h
+++ src/libs/WorkImage.h
@@ -6,7 +6,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
-#include <opencv2/core/core.hpp>
+#include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include "opencv2/objdetect/objdetect.hpp"
